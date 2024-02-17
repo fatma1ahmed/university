91476faccbe2c36d5142f0b38b61d6b2750ff590
MoaazSuliman <moaazsuliman1@gmail.com>
2024-02-17 18:46:33 +0200

    added some logs in the main class

--- src/main/java/com/fatma/university/UniversityApplication.java
+++ src/main/java/com/fatma/university/UniversityApplication.java
@@ -1,13 +1,16 @@
 package com.fatma.university;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class UniversityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UniversityApplication.class, args);
+		log.info("Running Successfully By Moaaz And Fatma...");
 	}
 
 }
